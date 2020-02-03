b6cfd49678cd68610c6ceba0de4f3d37de47fb35
roadelou <roadelou@delltbr>
2020-02-03 16:44:29 +0100

    Formatted the files using black for increased readability

--- gen.py
+++ gen.py
@@ -3,9 +3,9 @@ import pickle
 
 array = np.random.rand(10, 6)
 
-np.save('traj.npy', array)
-np.savetxt('traj.txt', array)
-np.savetxt('traj.csv', array, delimiter = ',')
-array.tofile('traj.dat')
-with open('traj', 'wb') as target:
+np.save("traj.npy", array)
+np.savetxt("traj.txt", array)
+np.savetxt("traj.csv", array, delimiter=",")
+array.tofile("traj.dat")
+with open("traj", "wb") as target:
     pickle.dump(array, target)
